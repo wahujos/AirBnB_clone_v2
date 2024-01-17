9052f1d65345cc8c8141fda782819d8463de4204
wahujos <jwahu30@gmail.com>
2024-01-17 05:17:03 +0000

    Commiting changes

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] == '{' and pline[-1] =='}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -113,8 +113,11 @@ class HBNBCommand(cmd.Cmd):
         """ Overrides the emptyline method of CMD """
         pass
 
+    '''
     def do_create(self, args):
-        """ Create an object of any class"""
+        """
+        Create an object of any class
+        """
         if not args:
             print("** class name missing **")
             return
@@ -125,6 +128,35 @@ class HBNBCommand(cmd.Cmd):
         storage.save()
         print(new_instance.id)
         storage.save()
+    '''
+    def do_create(self, arg):
+        """
+        Function that allows for object creation 
+        with given parameters
+        """
+        cname = arg.split(" ")[0]
+        if not cname:
+            print("** class name missing **")
+            return
+        if cname not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+            return
+        arg_list = arg.split(" ")
+        new_instance = globals()[cname]()
+        for i in range(1, len(arg_list)):
+            key, value = tuple(arg_list[i].split("="))
+            if value.startswith('"'):
+                value = value.strip('"').replace("_", " ")
+            elif '.' in value:
+                value = float(value)
+            else:
+                value = int(value)
+            if hasattr(new_instance, key):
+                setattr(new_instance, key, value)
+        storage.new(new_instance)
+        print(new_instance.id)
+        new_instance.save()
+
 
     def help_create(self):
         """ Help information for the create method """
@@ -272,7 +304,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -280,10 +312,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
 
 
 class FileStorage:
@@ -8,8 +16,16 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
+        if cls:
+            if cls and isinstance(cls, str):
+                cls = globals().get(cls)
+            if cls and issubclass(cls, BaseModel):
+                class_dict = {key: value for key, value in
+                              self.__objects.items()
+                              if isinstance(value, cls)}
+                return class_dict
         return FileStorage.__objects
 
     def new(self, obj):
@@ -27,14 +43,6 @@ class FileStorage:
 
     def reload(self):
         """Loads storage dictionary from file"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
-
         classes = {
                     'BaseModel': BaseModel, 'User': User, 'Place': Place,
                     'State': State, 'City': City, 'Amenity': Amenity,
@@ -45,6 +53,18 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
+            pass
+
+        # Question five updates
+
+    def delete(self, obj=None):
+        """public instance method to delete an object"""
+        if obj is None:
+            return
+        obj_for_delete = "{}.{}".format(obj.__class__.__name__, obj.id)
+        try:
+            del FileStorage.__objects[obj_for_delete]
+        except Exception:
             pass
