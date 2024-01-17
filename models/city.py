29482e70a3d5987c820ea68eb7a55b1e7fac5e83
Paul Kirugu <pkirugu.chege111@gmail.com>
2024-01-17 14:00:33 +0000

    Task 6: SqlAlchemy commit

--- models/city.py
+++ models/city.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+import models
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    __tablename__ = 'cities'
+    # Columns
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), FoerignKey('state.id'), nullable=False)
+    state = relationship('State', back_populates='cities)
