c491cfa207bbac19e2f882236226abb7d43a0a6b
wahujos <jwahu30@gmail.com>
2024-02-03 17:47:21 +0300

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,27 +3,20 @@
 
 sudo apt-get update
 sudo apt install -y nginx
-mkdir -p /data/
-mkdir -p /data/web_static/
-mkdir -p /data/web_static/releases/
+# mkdir -p /data/
+# mkdir -p /data/web_static/
+# mkdir -p /data/web_static/releases/
 mkdir -p /data/web_static/shared/
-mkdir -p /data/web_static/releases/test/
-echo "<h1> Testing nginx configurations </h1>" | sudo tee /data/web_static/releases/test/index.html
+mkdir -p /data/web_static/releases/test
+echo "<html>
+        <head>
+	</head>
+	<body>
+	  Holberton School
+        </body>
+ </html>" | sudo tee /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-domain="wahujos.tech"
-nginx_config="/etc/nginx/sites-available/default"
-temp_config="/tmp/nginx_temp_config"
-echo "server {
-	listen 80;
-	server_name $domain;
-
-	location /hbnb_static {
-		alias /data/web_static/current/;
-		index index.html;
-	}
-}" > "$temp_config"
-sudo cp /etc/nginx/sites-available/default /etc/nginx/sites-available/default_backup
-sudo mv "$temp_config" "$nginx_config"
+sudo sed -i '/server_name _;/a \        location \/hbnb_static {\n\     alias \/data\/web_static\/current\/;\n\ index index.html;\n\    }' /etc/nginx/sites-enabled/default
 sudo nginx -t
 sudo service nginx start
